e82a612eb928918c37667459cc9b74686d4da40f
Fabio <fabio.spaziani@ellycode.com>
2024-04-04 22:11:12 +0200

    add application

--- ML.Helpers.UI/Program.cs
+++ ML.Helpers.UI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ML.Helpers.UI;
+using ML.Helpers.UI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IClipboardService, ClipboardService>();
 
 await builder.Build().RunAsync();
--- ML.Helpers.UI/Services/ClipboardService.cs
+++ ML.Helpers.UI/Services/ClipboardService.cs
@@ -1,6 +1,23 @@
-﻿namespace ML.Helpers.UI.Services;
+﻿using Microsoft.JSInterop;
 
-public class ClipboardService
+namespace ML.Helpers.UI.Services;
+
+public class ClipboardService : IClipboardService
 {
-    
+    private readonly IJSRuntime _jsRuntime;
+
+    public ClipboardService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task CopyToClipboard(string text)
+    {
+        await _jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
+
+public interface IClipboardService
+{
+    Task CopyToClipboard(string text);
 }
